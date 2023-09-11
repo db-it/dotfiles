4847097963d7d0b83d5548b611cf51bc9b1ed359
Dominik Büttner <mail@db-it.de>
2023-09-11 17:25:38 +0200

    Add fish_greeting.fish

---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,10 @@
+function fish_greeting -d "What's up, fish?"
+    # this overwrites: ~/.local/share/omf/themes/bobthefish/fish_greeting.fish
+    # set_color $fish_color_autosuggestion
+    # echo "waketime:" (sysctl -a |grep waketime |cut -d " " -f 10-15)
+
+    # echo "node" (node --version)
+    # echo "npm" (npm --version)
+
+    # set_color normal
+end

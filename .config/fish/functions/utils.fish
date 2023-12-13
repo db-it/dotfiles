56ec9aac1f34ff78579f7991f27ce7c058ff2f15
Dominik Büttner <mail@db-it.de>
2023-12-13 12:09:33 +0100

    Consolidate funcs.fish with utils.fish

--- .config/fish/functions/utils.fish
+++ .config/fish/functions/utils.fish
@@ -1,3 +1,11 @@
-function lfext --description 'List file extensions sorted by occurrence. Typical development paths are excluded.'
+function lfext --description "List file extensions sorted by occurrence. Typical development paths are excluded."
   find . -type f ! -path './.git/*' ! -path './target/*' ! -path './.idea/*' | rev | cut -d. -f1 | rev  | tr '[:upper:]' '[:lower:]' | sort | uniq --count | sort -rn
+end
+
+
+function wtf --description "Print which and --version output for the given command"
+    for arg in $argv
+        echo $arg: (which $arg)
+        echo $arg: (sh -c "$arg --version")
+    end
 end

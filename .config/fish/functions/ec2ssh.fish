05409741a71589d769d0df4ef6d14f76d7bf93e4
Dominik Büttner <mail@db-it.de>
2023-12-13 12:29:21 +0100

    Change path to elasticbamboo ssh key

--- .config/fish/functions/ec2ssh.fish
+++ .config/fish/functions/ec2ssh.fish
@@ -1,10 +1,8 @@
-function ec2ssh --description 'connect to ec2 instance via ssh'
-
+function ec2ssh --description 'Connect to Bamboo EC2 instance via SSH'
     if test (count $argv) -lt 1
         echo "Error: Missing hostname of EC2 instance"
         return
     end
-    echo "ssh -i /var/opt/bamboo/shared/configuration/elasticbamboo.pk ubuntu@$argv[1]"
-    ssh -i /var/opt/bamboo/shared/configuration/elasticbamboo.pk ubuntu@$argv[1]
-
+    echo "ssh -i ~/.ssh/elasticbamboo.pk ubuntu@$argv[1]"
+    ssh -i ~/.ssh/elasticbamboo.pk ubuntu@$argv[1]
 end

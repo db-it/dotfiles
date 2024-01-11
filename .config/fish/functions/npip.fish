13127b0770e31d2f837a63fb196b23ee642f48af
Dominik Büttner <mail@db-it.de>
2024-01-11 06:26:45 +0100

    Fix npip fish function

--- .config/fish/functions/npip.fish
+++ .config/fish/functions/npip.fish
@@ -1,4 +1,3 @@
 function npip --description 'Pip defining an extra index'
-  pip3 install --extra-index-url "(cat ~/.secrets/nextus_pypi_extra_index_url)" "$argv"
+  pip3 install --extra-index-url "$(cat ~/.secrets/nextus_pypi_extra_index_url)" "$argv"
 end
-

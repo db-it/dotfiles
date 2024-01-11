5e9bbbe2069672cbb9ed66afed9c56e555dc4193
Dominik Büttner <mail@db-it.de>
2024-01-11 07:46:16 +0100

    Improve fish function npip

--- .config/fish/functions/npip.fish
+++ .config/fish/functions/npip.fish
@@ -1,3 +1,7 @@
-function npip --description 'Pip defining an extra index'
-  pip3 install --extra-index-url "$(cat ~/.secrets/nextus_pypi_extra_index_url)" "$argv"
+function npip --description 'pip defining an extra index'
+  if test "$argv[1]" = "install"
+    pip3 $argv[1] --extra-index-url "$(cat ~/.secrets/nextus_pypi_extra_index_url)" "$argv[2..-1]"
+  else
+    pip3 "$argv"
+  end
 end

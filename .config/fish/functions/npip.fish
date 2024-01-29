eddec9bb083f21a2e889d78dad11edf6a2d5c9fc
Dominik Büttner <mail@db-it.de>
2024-01-29 13:28:28 +0100

    Fix quoting issue on fish function npip

--- .config/fish/functions/npip.fish
+++ .config/fish/functions/npip.fish
@@ -1,7 +1,7 @@
 function npip --description 'pip defining an extra index'
   if test "$argv[1]" = "install"
-    pip3 $argv[1] --extra-index-url "$(cat ~/.secrets/nextus_pypi_extra_index_url)" "$argv[2..-1]"
+    pip3 $argv[1] --extra-index-url "$(cat ~/.secrets/nextus_pypi_extra_index_url)" $argv[2..-1]
   else
-    pip3 "$argv"
+    pip3 $argv
   end
 end

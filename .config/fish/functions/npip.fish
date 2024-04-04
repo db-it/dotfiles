f52540c3014a831467c068d17c7cac706a4cbcf6
Dominik Büttner <mail@db-it.de>
2024-04-04 09:14:48 +0200

    Print path to python executable when using npip

--- .config/fish/functions/npip.fish
+++ .config/fish/functions/npip.fish
@@ -1,4 +1,6 @@
 function npip --description 'pip defining an extra index'
+  echo "Using python: $(pyenv which python3)"
+  echo ""
   if test "$argv[1]" = "install"
     python3 -m pip $argv[1] --extra-index-url "$(cat ~/.secrets/nextus_pypi_extra_index_url)" $argv[2..-1]
   else

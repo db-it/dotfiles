035e074fc2e92112258ea835028479fe9cb5b6e0
Dominik Büttner <mail@db-it.de>
2024-11-18 14:35:46 +0100

    Don not init git repo when running d12r_git_init

--- .config/fish/functions/d12r_git_init.fish
+++ .config/fish/functions/d12r_git_init.fish
@@ -1,5 +1,5 @@
 function d12r_git_init --description 'Init Git directory and set author information'
-    git init
+    # git init
     echo "git config --local user.name 'Dominik Büttner'"
     git config --local user.name "Dominik Büttner"
     echo "git config --local user.email 'mail@db-it.de'"

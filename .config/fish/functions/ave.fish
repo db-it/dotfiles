322450c6e5ecc431b8795f940f567fe7c8096ae0
Dominik Büttner <mail@db-it.de>
2025-02-17 08:03:10 +0100

    Add argument envname for activating different envs

--- .config/fish/functions/ave.fish
+++ .config/fish/functions/ave.fish
@@ -1,3 +1,10 @@
-function ave --description 'Activate venv'
-    source venv/bin/activate.fish
+function ave --argument-names envname --description 'Activate a Python virtual environment. Default envname is "venv"'
+    if test -z "$envname"
+        set envname "venv"
+    end
+
+    source $envname/bin/activate.fish
+    if [ "$status" -eq 0 ]
+        echo "Activated $envname"
+    end
 end
